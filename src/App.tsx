88220e4f2c9a4368a92a7cde897c33ebebeed132
Sebastian Guaqueta <sebasguaqueta@Sebastians-MacBook-Pro.local>
2023-08-06 08:23:48 -0500

    adding chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
-
+import { ChakraProvider } from "@chakra-ui/react";
 import { Attestooooooor } from "./components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/loginPage";
 import HomePage from "./pages/homePage";
+import Navbar from "./components/navBar";
 export function App() {
   /**
    * Wagmi hook for getting account information
@@ -17,7 +18,6 @@ export function App() {
     //   <h1>OP Starter Project</h1>
 
     //   {/** @see https://www.rainbowkit.com/docs/connect-button */}
-    //   <ConnectButton />
 
     //   {isConnected && (
     //     <>
@@ -27,11 +27,14 @@ export function App() {
     //     </>
     //   )}
     // </>
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
 }
