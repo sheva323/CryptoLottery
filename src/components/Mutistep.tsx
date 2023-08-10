d0e85f48824ffc9a13ffc91888a702e2ad878fed
gitguti <gitmelgtrrz1@gmail.com>
2023-08-09 22:08:54 -0400

    created layout and multistep form

---
+++ src/components/Mutistep.tsx
@@ -0,0 +1,143 @@
+'use client'
+
+import { useState } from 'react'
+import {
+  Progress,
+  Box,
+  ButtonGroup,
+  Button,
+  Heading,
+  Flex,
+  FormControl,
+  GridItem,
+  FormLabel,
+  Input,
+  Grid,
+  Select,
+  SimpleGrid,
+  InputLeftAddon,
+  InputGroup,
+  Textarea,
+  FormHelperText,
+  InputRightElement,
+} from '@chakra-ui/react';
+import Buy from './buy';
+import Projects from './projects';
+import { useToast } from '@chakra-ui/react'
+
+const Form1 = () => {
+  const [show, setShow] = useState(false)
+  const handleClick = () => setShow(!show)
+  return (
+    <>
+      <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%">
+        User Registration
+      </Heading>
+      <Flex>
+        <Buy />
+      </Flex>
+    </>
+  )
+}
+
+const Form2 = () => {
+  return (
+    <>
+      <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%">
+        User Details
+      </Heading>
+      <Grid templateColumns='repeat(5, 1fr)' gap={6}>
+        <Projects />
+        <Projects />
+        <Projects />
+        <Projects />
+        <Projects />
+        <Projects />
+        <Projects />
+      </Grid>
+    </>
+  )
+}
+
+const Form3 = () => {
+  return (
+    <>
+      <Heading w="100%" textAlign={'center'} fontWeight="normal">
+        Social Handles
+      </Heading>
+      <Heading>Agarra tu nft</Heading>
+    </>
+  )
+}
+
+export default function Multistep() {
+  const toast = useToast()
+  const [step, setStep] = useState(1)
+  const [progress, setProgress] = useState(33.33)
+
+  return (
+    <>
+      <Box
+        borderWidth="1px"
+        rounded="lg"
+        shadow="1px 1px 3px rgba(0,0,0,0.3)"
+        maxWidth={800}
+        p={6}
+        m="10px auto"
+        as="form">
+        <Progress hasStripe value={progress} mb="5%" mx="5%" isAnimated></Progress>
+        {step === 1 ? <Form1 /> : step === 2 ? <Form2 /> : <Form3 />}
+        <ButtonGroup mt="5%" w="100%">
+          <Flex w="100%" justifyContent="space-between">
+            <Flex>
+              <Button
+                onClick={() => {
+                  setStep(step - 1)
+                  setProgress(progress - 33.33)
+                }}
+                isDisabled={step === 1}
+                colorScheme="teal"
+                variant="solid"
+                w="7rem"
+                mr="5%">
+                Back
+              </Button>
+              <Button
+                w="7rem"
+                isDisabled={step === 3}
+                onClick={() => {
+                  setStep(step + 1)
+                  if (step === 3) {
+                    setProgress(100)
+                  } else {
+                    setProgress(progress + 33.33)
+                  }
+                }}
+                colorScheme="teal"
+                variant="outline">
+                Next
+              </Button>
+            </Flex>
+            {step === 3 ? (
+              <Button
+                w="7rem"
+                colorScheme="red"
+                variant="solid"
+                onClick={() => {
+                  toast({
+                    title: 'Account created.',
+                    description: "We've created your account for you.",
+                    status: 'success',
+                    duration: 3000,
+                    isClosable: true,
+                  })
+                }}>
+                Submit
+              </Button>
+            ) : null}
+          </Flex>
+        </ButtonGroup>
+      </Box>
+    </>
+  )
+}
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -2,18 +2,42 @@ import React from "react";
 import Counter from '../components/counter';
 import Buy from '../components/buy';
 import Projects from '../components/projects';
+import Multistep from "../components/Mutistep";
 
 import {
   Flex, Center, Text, Square, Box, Spacer, Heading, HStack, Button, Stack, Avatar, VStack, Input, InputGroup, InputLeftAddon, Badge, Link,
-  useColorModeValue,
+  useColorModeValue, Grid, GridItem
 } from '@chakra-ui/react';
 
 const HomePage = () => {
   return <>
-    <div>test home</div>
-    {/* <Counter />
+    {/* <div>test home</div> */}
+    {/*
     <Buy /> */}
     {/* <Projects /> */}
+    <Grid
+      templateAreas={`"header header"
+                  "main main"
+                  "footer footer"`}
+      gridTemplateRows={'auto 1fr auto'}
+      gridTemplateColumns={'150px 1fr'}
+      h='200px'
+      gap='1'
+      color='blackAlpha.700'
+      fontWeight='bold'
+      maxW='70%'
+      margin="0 auto"
+    >
+      <GridItem pl='2' bg='orange.300' area={'header'}>
+        <Counter />
+      </GridItem>
+      <GridItem pl='2' bg='green.300' area={'main'}>
+        <Multistep />
+      </GridItem>
+      <GridItem pl='2' bg='blue.300' area={'footer'}>
+        Footer
+      </GridItem>
+    </Grid>
 
   </>
 };
