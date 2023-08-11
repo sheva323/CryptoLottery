722a7d15cb746bb4eb19fc4c53e33a096cf7ba0d
Sebastian Guaqueta <sebasguaqueta@Sebastians-MacBook-Pro.local>
2023-08-10 21:19:29 -0500

    working on unit tests

--- contracts/test/SuperLotoV2.t.sol
+++ contracts/test/SuperLotoV2.t.sol
@@ -66,6 +66,63 @@ contract SuperLotoTest is Test {
     // You can check playerTickets and beneficiary mappings
   }
 
+  function testBuyTicketMultipleWithDifferentBeneficiaries() public {
+    uint256 ticketCost = 100;
+    address beneficiary1 = address(0x123); // Replace with actual beneficiary addresses
+    address beneficiary2 = address(0x456);
+    address beneficiary3 = address(0x789);
+
+    // Crear un nuevo juego
+    superLoto.startNewGame(ticketCost, 5, 7, 100, 5);
+
+    // Comprar tres boletos con diferentes beneficiarios
+    superLoto.value(ticketCost * 3).buyTicket(beneficiary1);
+    superLoto.value(ticketCost * 2).buyTicket(beneficiary2);
+    superLoto.value(ticketCost).buyTicket(beneficiary3);
+
+    // Obtener la información del juego recién creado desde el mapeo "lotteries"
+    SuperLoto.Lottery memory currentGame = superLoto.lotteries(
+      superLoto.gameIndex()
+    );
+
+    // Verificar que el contrato balanceBag se haya actualizado correctamente
+    assertEq(
+      currentGame.balanceBag,
+      ticketCost * 6,
+      "Balance bag should match total ticket cost"
+    );
+
+    // Verificar que los jugadores se hayan registrado en los playerTickets
+    assertEq(
+      currentGame.playerTickets.length,
+      6,
+      "Player ticket count should be 6"
+    );
+    // Verificar que los jugadores tengan los beneficiarios correctos
+    assertEq(
+      superLoto.beneficiary(superLoto.gameIndex(), address(this)),
+      beneficiary1,
+      "Beneficiary should match for player 1"
+    );
+    assertEq(
+      superLoto.beneficiary(superLoto.gameIndex(), address(this, 1)),
+      beneficiary2,
+      "Beneficiary should match for player 2"
+    );
+    assertEq(
+      superLoto.beneficiary(superLoto.gameIndex(), address(this, 2)),
+      beneficiary3,
+      "Beneficiary should match for player 3"
+    );
+
+    // Verificar que el contrato balance haya aumentado en el valor total de los boletos
+    assertEq(
+      address(superLoto).balance,
+      ticketCost * 6,
+      "Contract balance should match total ticket cost"
+    );
+  }
+
   function testFinishGame() public {
     uint256 ticketCost = 100;
     address beneficiary = address(0); // Replace with actual beneficiary address
